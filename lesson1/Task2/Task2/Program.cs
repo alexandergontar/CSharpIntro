3270e1e9aba695c2080a4e4b40674b9ffc37bbdc
agontar <agontar@yandex.ru>
2022-10-14 08:29:11 +0300

    add Task2

---
+++ lesson1/Task2/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        /// <summary>
+        /// Находим максимум из 3-х чисел
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+             // ввод
+            Console.Write("Введите число A: ");
+            int numA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число B: ");
+            int numB = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число C: ");
+            int numC = Convert.ToInt32(Console.ReadLine());
+            // вывод результата
+            Console.Write("Максимальное число: ");
+            if (numA > numB && numA > numC) 
+            { 
+                Console.WriteLine(numA);
+            }
+            else if (numB > numA && numB > numC) 
+            { 
+                Console.WriteLine(numB);
+            }
+            else 
+            { 
+                Console.WriteLine(numC);
+            } 
+            Console.ReadKey();
+        }
+    }
+}
