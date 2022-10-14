c17292fb92d1e00815401047d6015b6a1621b595
agontar <agontar@yandex.ru>
2022-10-14 08:49:02 +0300

    add Task3

---
+++ lesson1/Task3/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        /// <summary>
+        /// определение четности числа
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N % 2 == 0)
+            { 
+                Console.WriteLine($"Введенное число {N} - четное");
+            }
+            else 
+            {
+                Console.WriteLine($"Введенное число {N} - нечетное");
+            }
+            Console.ReadKey();
+        }
+    }
+}
