9c9e641395d0f1d6f5c8bf2ec015868ea04f8220
agontar <agontar@yandex.ru>
2022-10-14 09:02:26 +0300

    add Task4

---
+++ lesson1/Task4/Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        /// <summary>
+        /// определение четности чисел от 1 до N
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int i = 1; // начинаем счет с 1
+
+            while (i <= N)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i +" - четное");
+                }
+                else
+                {
+                    Console.WriteLine(i + " - нечетное");
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
