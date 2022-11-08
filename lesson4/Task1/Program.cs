d19f4099356e01cfb56efae7785556595fc4556a
agontar <agontar@yandex.ru>
2022-11-08 09:35:50 +0300

    add Task1 & Task2

---
+++ lesson4/Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+
+        static int GetInputNumber()
+        {
+            int inputNumber;
+            while (!Int32.TryParse(Console.ReadLine(), out inputNumber))
+            {
+                Console.Beep();
+                Console.Write("Неверный ввод, повторите: ");
+            }
+            return inputNumber;
+        }
+        /// <summary>
+        /// умножаем 1 на число N-раз
+        /// number ^ power = (1 * number * number ....) - power раз
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="power"></param>
+        /// <returns>(1 * number * number ....) - power раз</returns>
+        static int GetPower(int number, int power) 
+        {
+            int product = 1;
+            for (int i = 0; i < power; i++)
+            {
+                product *= number;
+            }            
+            return product;
+        }
+        /// <summary>
+        /// возведение в степень циклом
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int number, power;
+            Console.Write("Введите число: ");
+            number = GetInputNumber();           
+            Console.Write("Введите степень: ");
+            power = GetInputNumber();
+
+            int result = GetPower(number, power);
+
+            Console.WriteLine($" {number} ^ {power} = {result} ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson4/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static int GetInputNumber()
+        {
+            int inputNumber;
+            while (!Int32.TryParse(Console.ReadLine(), out inputNumber))
+            {
+                Console.Beep();
+                Console.Write("Неверный ввод, повторите: ");
+            }
+            return inputNumber;
+        }
+
+        static int SumOfDigits(int number) 
+        {
+            int sum = 0;
+            if (number < 0) number = -number;
+            while (number > 0) // пока в числе остаются цифры
+            {
+                sum = sum + number % 10; //приплюсовываем последнюю цифру числа
+                number = number / 10; // убираем последнюю цифру
+            }          
+            return sum;
+        }
+        /// <summary>
+        /// вычисляем сумму цифр в числе
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Write(" Введите число: ");
+            int number = GetInputNumber();
+
+            Console.WriteLine($" Сумма цифр в числе: {SumOfDigits(number)}");
+            Console.ReadKey();
+        }
+    }
+}
