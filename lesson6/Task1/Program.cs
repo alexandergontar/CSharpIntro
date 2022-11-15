ac8cd2c6a50f7aea04ecabf2b7412ccafb0d30f4
agontar <agontar@yandex.ru>
2022-11-15 10:47:26 +0300

    add Task1 & Task2 to lesson6

---
+++ lesson6/Task1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        /// <summary>
+        /// подсчет числел > 0 рекурсией
+        /// </summary>
+        /// <param name="array">входной массив</param>
+        /// <param name="index">индексатор</param>
+        /// <param name="countPos">счетчик положительных чисел</param>
+        /// <returns>countPos</returns>
+        static int CountPositivRecursive(int[] array, int index, int countPos)//метод определяет количество чисел больше 0.
+        {
+            if (index < 0) return countPos; //условие заверешния рекурсии
+            else if (array[index] <= 0) return CountPositivRecursive(array, index - 1, countPos);
+            else return CountPositivRecursive(array, index - 1, countPos + 1);
+        }
+        /// <summary>
+        /// простой счетчик положительных чисел
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns>countPositive</returns>
+        static int CountPositive(int [] array) 
+        {
+            int countPositive = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    countPositive++;
+                }
+            }
+            return countPositive;
+        }
+        static void DisplayArray<T>(T[] array)
+        {
+            Console.Write("[");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+                else Console.Write($"{array[i].ToString()}");
+            }
+            Console.Write("]\n");
+        }
+        /// <summary>
+        /// получает числосой массив из строки числовых символов,
+        /// разделенных пробелами или запятыми и пробелами, пустые
+        /// строки удаляются
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>числовой массив</returns>
+        static int[] InputArray(string message)
+        {
+            int[] numbers;
+            int pars;
+            Console.WriteLine(message);
+            // возвращает строковый массив без пустых строк
+            numbers = Console.ReadLine().Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries)
+                // Linq-запрос и лямда выражение преобразуют числовые символы в числа, в неуспешном случае подставляют нули
+                .Select(e => int.TryParse(e, out pars) ? pars : 0).ToArray(); // и преобразуют в массив
+            return numbers;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] numbers = InputArray("Введите числа через пробелы или запятые: ");
+            DisplayArray<int>(numbers);
+            Console.WriteLine("Подсчет положительных чисел метеодом рекурсии : " + CountPositivRecursive(numbers, numbers.Length - 1, 0) + "\n  ");
+            Console.WriteLine("Подсчет положительных чисел : " + CountPositive(numbers));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lesson6/Task2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static int GetInputNumber(string promptMessage)
+        {
+            Console.Write(promptMessage);
+            int inputNumber;
+            while (!Int32.TryParse(Console.ReadLine(), out inputNumber))
+            {
+                Console.Beep();
+                Console.Write("Неверный ввод, повторите: ");
+            }
+            return inputNumber;
+        }
+        /// <summary>
+        /// Находит точку пересечени двух линий: y = k1 * x + b1, y = k2 * x + b2 
+        /// </summary>
+        /// <param name="k1"></param>
+        /// <param name="k2"></param>
+        /// <param name="b1"></param>
+        /// <param name="b2"></param>
+        /// <returns>х и у - координаты точки пересечения</returns>
+        static (double x, double y) LinearEquation(int k1, int k2, int b1, int b2)//Метод решает линейные уравнения
+        {
+
+            if (k1 != k2)
+            {               
+                double x = (double)(b1 - b2) / (double)(k2 - k1);
+                double y = x * k1 + b1;
+                x = Math.Round(x, 2);
+                y = Math.Round(y, 2);
+                return (x, y);
+            }
+            else
+            {
+                Console.WriteLine("k1 = k2 - Уравнение не имеет решения");
+                Console.ReadKey();
+                return (0, 0);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== Решение линейного уравнения. =========");
+            Console.WriteLine("Введите 4 числа - \n" +
+    "значения b1, k1, b2 и k2 в уравнениях y = k1 * x + b1, y = k2 * x + b2 : ");
+            int k1 = GetInputNumber("k1 = ");
+            int k2 = GetInputNumber("k2 = ");
+            int b1 = GetInputNumber("b1 = ");
+            int b2 = GetInputNumber("b2 = ");
+            (double x, double y) = LinearEquation(k1, k2, b1, b2);
+            Console.WriteLine($"Координаты точки пересечения: x = {x} ; y = {y}");
+            Console.ReadKey();
+        }
+    }
+}
