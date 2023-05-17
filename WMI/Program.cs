cc3f748d572d3fad31c67bad300c2e02e46d6919
agontar <agontar@yandex.ru>
2023-05-17 11:09:01 +0300

    add Services to WMI project

--- WMI/Program.cs
+++ WMI/Program.cs
@@ -4,20 +4,57 @@ using System.Collections.Generic;
 using WMI.Model;
 using WMI.Services;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace WMI
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task<string> sendPost(string output) 
         {
-            
+            var data = new StringContent(output, Encoding.UTF8, "application/json");
+            var url = "https://httpbin.org/post";
+            using var client = new HttpClient();
+
+            var response = await client.PostAsync(url, data);
+
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+            return result;
+        }
+        static  void Main(string[] args)
+        {
+            //var http = new HttpClient();
             CompInfo info = new CompInfo();
             PC pc = info.GetInfo();
             string output = JsonConvert.SerializeObject(pc);
-            Console.WriteLine(output);
-            Console.ReadKey();
+            PC pc1 = JsonConvert.DeserializeObject<PC>(output);
+            foreach (Disk disk in pc1.Disks)
+            {
+                Console.WriteLine("{0}   {1}   {2}",
+                    disk.DiskName, disk.VolumeSize, disk.FreeSpace);
+            }
 
+            Console.WriteLine("\n ===== JSON Format ===== :");
+            Console.WriteLine(output);
+
+
+            /*var data = new StringContent(output, Encoding.UTF8, "application/json");
+            var url = "https://httpbin.org/post";
+            using var client = new HttpClient();
+
+            var response = await client.PostAsync(url, data);
+
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);*/
+             Task<string> result = sendPost(output);
+            result.Wait();
+            //await result;
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
--- WMI/Services/CompInfo.cs
+++ WMI/Services/CompInfo.cs
@@ -5,49 +5,27 @@ using WMI.Model;
 
 namespace WMI.Services
 {
-    public class CompInfo
-    {
-
+    public class CompInfo   {
+       
         public PC GetInfo() 
-        {
-            //ConnectionOptions connection = new ConnectionOptions();
-            //connection.Username = "cstgontaa";
-            //connection.Password = "N@talie-1917";
-            // connection.Authority = "";
-            // Build an options object for the remote connection
-            // if you plan to connect to the remote
-            // computer with a different user name
-            // and password than the one you are currently using.
-            // This example uses the default values.
+        {            
             List<Disk> disks = new List<Disk>();
             List<string> commonInfo = new List<string>();
             string compInfo =String.Empty;
             PC pc = new PC();
             try
             {
-                ConnectionOptions options =
-
-                               new ConnectionOptions();
-
-                //options.Username = "Alexander";
-                //options.Password = "12345";
-                //options.EnablePrivileges = true;
-                //options.Authority = "ntlmdomain:WORKGROUP";
-                //options.Authentication = AuthenticationLevel.Default;
-
+                ConnectionOptions options = new ConnectionOptions();
+                
+               /* options.EnablePrivileges = true;
                 options.Impersonation = ImpersonationLevel.Impersonate;
-
-
-                // Make a connection to a remote computer.
-                // Replace the "FullComputerName" section of the
-                // string "\\\\FullComputerName\\root\\cimv2" with
-                // the full computer name or IP address of the
-                // remote computer.
-                ManagementScope scope =
-
-                    new ManagementScope(
+                options.Authentication = AuthenticationLevel.Default;
+                options.Authority = "ntlmdomain:pet.corp.jti.com";
+                options.Username = "cstgontaa";
+                options.Password = "N@talie-1917";*/
+                
+                ManagementScope scope = new ManagementScope(
                     "\\\\localhost\\root\\cimv2", options);
-
                 scope.Connect();
 
                 //Query system for Operating System information
@@ -60,35 +38,28 @@ namespace WMI.Services
                 foreach (ManagementObject m in queryCollection)
                 {
                     // Display the remote computer information
-                    Console.WriteLine("Computer Name : {0}", m["csname"]);                   
+                    //Console.WriteLine("Computer Name : {0}", m["csname"]);                   
                     commonInfo.Add(String.Format("Computer Name : {0}\n", m["csname"]));
-                    Console.WriteLine("Windows Directory : {0}", m["WindowsDirectory"]);
+                   // Console.WriteLine("Windows Directory : {0}", m["WindowsDirectory"]);
                     commonInfo.Add(String.Format("Windows Directory : {0}\n", m["WindowsDirectory"]));
-                    Console.WriteLine("Operating System: {0}", m["Caption"]);
+                   // Console.WriteLine("Operating System: {0}", m["Caption"]);
                     commonInfo.Add(String.Format("Operating System: {0}\n", m["Caption"]));
-                    Console.WriteLine("Version: {0}", m["Version"]);
+                   // Console.WriteLine("Version: {0}", m["Version"]);
                     commonInfo.Add(String.Format("Version: {0}\n", m["Version"]));
-                    Console.WriteLine("Manufacturer : {0}", m["Manufacturer"]);
+                   // Console.WriteLine("Manufacturer : {0}", m["Manufacturer"]);
                     commonInfo.Add(String.Format("Manufacturer : {0}\n", m["Manufacturer"]));
-                }   
-
+                } 
 
                 query = new ObjectQuery("SELECT * FROM Win32_LogicalDisk");
-
                 searcher = new ManagementObjectSearcher(scope, query);
-
-
                 foreach (ManagementObject managementObject in searcher.Get())
                 {
                     Disk disk = new Disk();
-                    Console.WriteLine("Drive Name :" +
-                                       managementObject["Name"].ToString());
+                    //Console.WriteLine("Drive Name :" + managementObject["Name"].ToString());
                     disk.DiskName = managementObject["Name"].ToString();
-                    Console.WriteLine("Volume Size :" +
-                                       managementObject["Size"].ToString());
+                   // Console.WriteLine("Volume Size :" + managementObject["Size"].ToString());
                     disk.VolumeSize = (UInt64)managementObject["Size"];
-                    Console.WriteLine("Free Space :" +
-                                       managementObject["FreeSpace"].ToString());
+                   // Console.WriteLine("Free Space :" + managementObject["FreeSpace"].ToString());
                     disk.FreeSpace = (UInt64)managementObject["FreeSpace"];
                     disks.Add(disk);
                 }
@@ -98,14 +69,14 @@ namespace WMI.Services
                 }
                 foreach (string item in commonInfo)
                 {
-                    Console.WriteLine(item);
+                   // Console.WriteLine(item);
                     compInfo += item;
                 }
                 Console.Write(compInfo);
                 pc.Disks = disks;
                 pc.PcInfo = compInfo;
                 pc.PcItems = commonInfo;
-                Console.ReadKey();
+                //Console.ReadKey();
                 return pc;
             }
             catch (Exception ex)
@@ -118,14 +89,14 @@ namespace WMI.Services
                 }
                 foreach (string item in commonInfo)
                 {
-                    Console.WriteLine(item);
+                    //Console.WriteLine(item);
                     compInfo += item;
                 }
                 Console.Write(compInfo);
                 pc.Disks = disks;
                 pc.PcInfo = compInfo;
                 pc.PcItems = commonInfo;
-                Console.ReadKey();
+                //Console.ReadKey();
                 return pc;
             }
         }
